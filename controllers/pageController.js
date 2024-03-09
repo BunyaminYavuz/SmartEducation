4800a2275df77f330d049f96f37566739c85b71b
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2024-03-10 00:34:41 +0300

    Added contact page and established communication channel via email.

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,6 @@
+// Use at least Nodemailer v4.1.0
+const nodemailer = require('nodemailer');
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render('index', {
@@ -6,21 +9,74 @@ exports.getIndexPage = (req, res) => {
 };
 
 exports.getAboutPage = (req, res) => {
-    res.status(200).render('about', {
-      page: 'about',
-    });
-  };
+  res.status(200).render('about', {
+    page: 'about',
+  });
+};
 
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page: 'contact',
+  });
+};
+
+exports.mailSender = async (req, res) => {
+  try {
+    const outputMessage = `
+    
+    <h1>Mail Details </h1>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+    `;
+
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: 'example@gmail.com', // gmail account
+        pass: 'password', // gmail password
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Smart EDUCATION Contact Form" <example@gmail.com>', // sender address
+      to: 'example@gmail.com', // list of receivers
+      subject: 'Smart EDUCATION Contact Form New Message ✔', // Subject line
+      html: outputMessage, // html body
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+    // req.flash('success', 'We Received your message succesfully');
+
+    res.status(200).redirect('/contact');
+  } catch (err) {
+    //req.flash("error", `Something happened! ${err}`);
+    // req.flash('error', `Something happened!`);
+    console.log(err);
+    res.status(200).redirect('/contact');
+  }
+};
 
 exports.getRegisterPage = (req, res) => {
   res.status(200).render('register', {
-    page: 'register'
-  })
-}
+    page: 'register',
+  });
+};
 
 exports.getLoginPage = (req, res) => {
   res.status(200).render('login', {
-    page: 'login'
-  })
-}
-  
+    page: 'login',
+  });
+};
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.route('/').get(pageController.getIndexPage);
 router.route('/about').get(pageController.getAboutPage);
+router.route('/contact').get(pageController.getContactPage);
+router.route('/contact').post(pageController.mailSender);
 router
   .route('/register')
   .get(redirectMiddleware, pageController.getRegisterPage);
