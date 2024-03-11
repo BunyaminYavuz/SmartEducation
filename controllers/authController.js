a4cc1981fcc9f8897545872253d79fcc9c5cd303
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2024-03-11 14:56:07 +0300

    Validators for register and login endpoints are implemented.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,26 @@
+const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 const User = require('../models/User');
 const Category = require('../models/Category');
-const bcrypt = require('bcrypt');
 const Course = require('../models/Course');
 
 exports.createUser = async (req, res) => {
-  await User.create(req.body);
   try {
+    await User.create(req.body);
+
     res.status(201).render('login', {
       page: 'login',
     });
-  } catch {
-    res.status(400).json({
-      status: 'Failed',
-      error,
-    });
+  } catch (error) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      errors.array().forEach((error) => {
+        req.flash('error', `${error.msg}`);
+      });
+    }
+
+    res.status(400).redirect('/register');
   }
 };
 
@@ -21,20 +28,25 @@ exports.loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
+
+    if(user){
+      const same = await bcrypt.compare(password, user.password);
+      if (same) {
+        // User SESSION
+        req.session.userID = user._id;
+        res.status(200).redirect('/users/dashboard');
+      } else {
+        req.flash('error', 'Incorrect password. Please try again.');
+        res.status(400).redirect('/login');
+      }
+    }
     if (!user) {
-      res.status(400).send('No user');
+      req.flash('error', 'No user found with this email. Please check your email or register.');
+      res.status(400).redirect('/login');
     }
-    const same = await bcrypt.compare(password, user.password);
-    if (same) {
-      // User SESSION
-      req.session.userID = user._id;
-      res.status(200).redirect('/users/dashboard');
-    }
-  } catch {
-    res.status(400).json({
-      status: 'Failed',
-      error,
-    });
+  } catch (error) {
+    req.flash('error', 'Something went wrong!')
+    res.status(400).redirect('/login')
   }
 };
 
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,15 +1,44 @@
 const express = require('express');
+const { body } = require('express-validator');
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
-
+const User = require('../models/User');
 
 const router = express.Router();
 
-router.route('/signup').post(authController.createUser); // http://localhost:3000/users/signup
+router.route('/signup').post(
+  [
+    body('name')
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('Please enter your name!')
+      .isLength({ min: 2 })
+      .withMessage('Name must be at least 2 characters long'),
+
+    body('email')
+      .trim()
+      .isEmail()
+      .withMessage('Please enter a valid email!')
+      .custom(async (userMail) => {
+        const user = await User.findOne({ email: userMail });
+        if (user) {
+          throw new Error('This email is already in use!');
+        }
+      }),
+
+    body('password')
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('Please enter your password')
+      .isLength({ min: 6 })
+      .withMessage('Password must be at least 6 characters long'),
+  ],
+  authController.createUser
+); // http://localhost:3000/users/signup
 router.route('/login').post(authController.loginUser); // http://localhost:3000/users/login
 router.route('/logout').get(authController.logoutUser);
-router
-  .route('/dashboard')
-  .get(authMiddleware, authController.getDashboardPage);
+router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);
 
 module.exports = router;
